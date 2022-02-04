20bb5665b4c65f85ba010e3c9f09e2eddaadf1e6
David Asensio Cañas <david.asensio@icloud.com>
2022-02-04 23:14:38 +0100

    Separate effectRegistry from its test

---
+++ src/effectRegistry.js
@@ -0,0 +1,51 @@
+export class NotRegisteredEffectError extends Error {
+  constructor (message) {
+    super(message)
+    this.name = 'NotRegisteredEffectError'
+  }
+}
+
+export class MalformedEffectDescriptorError extends Error {
+  constructor (message) {
+    super(message)
+    this.name = 'MalformedEffectDescriptorError'
+  }
+}
+
+export function createEffectRegistry (dependencies = {}) {
+  const {
+    generator,
+    storage
+  } = dependencies
+
+  function create (effectFn) {
+    const id = generator.uniqueId(effectFn.name)
+    storage.set(id, effectFn)
+
+    return (...args) => ({ id, args })
+  }
+
+  function getFnByDescriptor (descriptor) {
+    validDescriptorOrTrow(descriptor)
+    effectExistsOrThrow(descriptor.id)
+
+    return storage.get(descriptor.id)
+  }
+
+  function effectExistsOrThrow (id) {
+    if (!storage.has(id)) {
+      throw new NotRegisteredEffectError(`The effect identified by "${id}" is not registered`)
+    }
+  }
+
+  function validDescriptorOrTrow (descriptor) {
+    if (!descriptor?.id) {
+      throw new MalformedEffectDescriptorError('Malformed effect descriptor, effect descriptors must contain an id')
+    }
+  }
+
+  return {
+    create,
+    getFnByDescriptor
+  }
+}
--- src/effectsRegistry.test.js
+++ src/effectsRegistry.test.js
@@ -1,4 +1,5 @@
 import { when } from 'jest-when'
+import { createEffectRegistry, MalformedEffectDescriptorError, NotRegisteredEffectError } from './effectRegistry'
 
 const generator = {
   uniqueId: jest.fn()
@@ -107,55 +108,3 @@ describe('getFnByDescriptor', () => {
     )
   })
 })
-
-class NotRegisteredEffectError extends Error {
-  constructor (message) {
-    super(message)
-    this.name = 'NotRegisteredEffectError'
-  }
-}
-
-class MalformedEffectDescriptorError extends Error {
-  constructor (message) {
-    super(message)
-    this.name = 'MalformedEffectDescriptorError'
-  }
-}
-
-function createEffectRegistry (dependencies = {}) {
-  const {
-    generator,
-    storage
-  } = dependencies
-
-  function create (effectFn) {
-    const id = generator.uniqueId(effectFn.name)
-    storage.set(id, effectFn)
-
-    return (...args) => ({ id, args })
-  }
-
-  function getFnByDescriptor (descriptor) {
-    validDescriptorOrTrow(descriptor)
-    effectExistsOrThrow(descriptor.id)
-
-    return storage.get(descriptor.id)
-  }
-
-  function effectExistsOrThrow (id) {
-    if (!storage.has(id)) {
-      throw new NotRegisteredEffectError(`The effect identified by "${id}" is not registered`)
-    }
-  }
-
-  function validDescriptorOrTrow (descriptor) {
-    if (!descriptor?.id) {
-      throw new MalformedEffectDescriptorError('Malformed effect descriptor, effect descriptors must contain an id')
-    }
-  }
-
-  return {
-    create,
-    getFnByDescriptor
-  }
-}
