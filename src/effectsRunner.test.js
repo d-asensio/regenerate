a033e7c891f22e080ddee337ba812c7f3b89b08f
David Asensio Cañas <david.asensio@icloud.com>
2022-02-06 00:06:21 +0100

    The effectsRunner gets an effect function from the registry by its descriptor and executes it

---
+++ src/effectsRunner.test.js
@@ -0,0 +1,48 @@
+import { when } from 'jest-when'
+
+const effectRegistry = {
+  getFnByDescriptor: jest.fn()
+}
+
+describe('run', () => {
+  const effectsRunner = createEffectsRunner({ effectRegistry })
+
+  it('should get the first effect function and execute it with the arguments provided by the effect descriptor', () => {
+    const firstEffectArguments = [
+      'an-argument',
+      'another-argument'
+    ]
+    const firstEffectDescriptor = {
+      id: 'a-effect-id',
+      args: firstEffectArguments
+    }
+    const firstEffectFn = jest.fn()
+    when(effectRegistry.getFnByDescriptor)
+      .calledWith(firstEffectDescriptor)
+      .mockReturnValue(firstEffectFn)
+    const effects = [firstEffectDescriptor]
+
+    effectsRunner.run(
+      effects[Symbol.iterator]()
+    )
+
+    expect(firstEffectFn).toHaveBeenCalledWith(...firstEffectArguments)
+  })
+})
+
+function createEffectsRunner (dependencies = {}) {
+  const {
+    effectRegistry
+  } = dependencies
+
+  function run (effectStream) {
+    const iteratee = effectStream.next()
+
+    const effectDescriptor = iteratee.value
+    const effectFn = effectRegistry.getFnByDescriptor(effectDescriptor)
+
+    effectFn(...effectDescriptor.args)
+  }
+
+  return { run }
+}
