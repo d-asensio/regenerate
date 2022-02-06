59e9ba1f5baf4ef271cfb6af85da45c74259ff05
David Asensio Cañas <david.asensio@icloud.com>
2022-02-06 14:52:02 +0100

    EffectDescriptor DTO can be created with effect arguments

--- src/effectDescriptor.test.js
+++ src/effectDescriptor.test.js
@@ -1,21 +1,38 @@
 describe('constructor', () => {
-  it('should construct an EffectDescriptor from and id', () => {
+  it('should construct an EffectDescriptor with a effect id', () => {
     const effectId = 'a-effect-id'
 
     const effectDescriptor = new EffectDescriptor(effectId)
 
     expect(effectDescriptor.id).toBe(effectId)
   })
+
+  it('should construct an EffectDescriptor with arguments', () => {
+    const effectArguments = [
+      'an-argument',
+      'another-argument'
+    ]
+
+    const effectDescriptor = new EffectDescriptor('a-effect-id', effectArguments)
+
+    expect(effectDescriptor.args).toBe(effectArguments)
+  })
 })
 
 class EffectDescriptor {
   #id;
+  #args;
 
-  constructor (id) {
+  constructor (id, args) {
     this.#id = id
+    this.#args = args
   }
 
   get id () {
     return this.#id
+  }
+
+  get args () {
+    return this.#args
   }
 }
