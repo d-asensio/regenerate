bc1dc3db0e87532ca03cb7623ffeb53f78b0af22
David Asensio Cañas <david.asensio@icloud.com>
2022-02-06 20:22:57 +0100

    Create effect executor module to handle effects

--- src/effectsRunner.test.js
+++ src/effectsRunner.test.js
@@ -102,6 +102,63 @@ describe('run', () => {
   })
 })
 
+describe('exec', () => {
+  const effectExecutor = createEffectExecutor({ effectRegistry })
+
+  it('should get an effect from the repository by id and execute it with the arguments provided by the descriptor', () => {
+    const effectId = 'a-effect-id'
+    const effectArguments = [
+      'an-argument',
+      'another-argument'
+    ]
+    const effectFn = jest.fn()
+    const effectDescriptor = EffectDescriptor.fromObject({
+      id: effectId,
+      args: effectArguments
+    })
+    when(effectRegistry.getEffectById)
+      .calledWith(effectId)
+      .mockReturnValue(effectFn)
+
+    effectExecutor.exec(effectDescriptor)
+
+    expect(effectFn).toHaveBeenCalledWith(...effectArguments)
+  })
+
+  it('should return the effect result', () => {
+    const effectId = 'a-effect-id'
+    const effectDescriptor = EffectDescriptor.fromObject({
+      id: effectId
+    })
+    const effectFn = jest.fn()
+    const effectResult = 'a-result'
+    when(effectFn)
+      .mockReturnValue(effectResult)
+    when(effectRegistry.getEffectById)
+      .calledWith(effectId)
+      .mockReturnValue(effectFn)
+
+    const result = effectExecutor.exec(effectDescriptor)
+
+    expect(result).toBe(effectResult)
+  })
+})
+
+function createEffectExecutor (dependencies = {}) {
+  const {
+    effectRegistry
+  } = dependencies
+
+  function exec (effectDescriptor) {
+    const { id, args } = effectDescriptor
+    const effectFn = effectRegistry.getEffectById(id)
+
+    return effectFn(...args)
+  }
+
+  return { exec }
+}
+
 function createEffectsRunner (dependencies = {}) {
   const {
     effectRegistry
