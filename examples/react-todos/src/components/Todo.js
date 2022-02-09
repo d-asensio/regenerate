f72f5a46a9840a4d53cdcc99b5a06df156b6c4cf
David Asensio Cañas <david.asensio@icloud.com>
2022-02-09 21:23:06 +0100

    (examples/react-todos) Subscribe individual todos to the store

--- examples/react-todos/src/components/Todo.js
+++ examples/react-todos/src/components/Todo.js
@@ -1,12 +1,19 @@
 import { Box, Button, Card, CheckBox, TextInput } from 'grommet'
 import { Edit, Save, Trash } from 'grommet-icons'
+import { useSelector } from '../store'
+import { todoSelector } from '../selectors/todos'
 
-export function Todo ({ text, completed, editing }) {
+export function Todo ({ id }) {
+  const editing = false
+  const { title, completed } = useSelector(
+    state => todoSelector(state, id)
+  )
+
   return (
     <Card direction="row" pad="medium" gap="medium" fill="horizontal" justify="between" align="center">
       <Box align="center" justify="center" direction="row" gap="small">
         <CheckBox checked={completed}/>
-        <TextInput value={text} plain={!editing} focusIndicator={!!editing}/>
+        <TextInput value={title} plain={!editing} focusIndicator={!!editing}/>
       </Box>
       <Box align="center" justify="center" direction="row" gap="xxsmall">
         <Button icon={editing ? <Save/> : <Edit/>}/>
