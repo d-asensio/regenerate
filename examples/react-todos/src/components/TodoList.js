b040edefb7fc6b71eadb80e9c2417b60cca2a0d3
David Asensio Cañas <david.asensio@icloud.com>
2022-02-09 21:06:56 +0100

    (examples/react-todos) TodoList renders a todo for each todo in the state

--- examples/react-todos/src/components/TodoList.js
+++ examples/react-todos/src/components/TodoList.js
@@ -1,11 +1,16 @@
 import { Box } from 'grommet'
 import { Todo } from './Todo'
+import { useSelector } from '../store'
+import { todoIdListSelector } from '../selectors/todos'
 
 export function TodoList () {
+  const todoIdList = useSelector(todoIdListSelector)
+
   return (
     <Box fill="horizontal" align="center" flex="grow" gap="medium" pad="small">
-      <Todo text={'Do thing'} editing/>
-      <Todo text={'Do another thing'} completed/>
+      {todoIdList.map(todoId => (
+        <Todo key={todoId} id={todoId} />
+      ))}
     </Box>
   )
 }
