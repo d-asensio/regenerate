6191efe977866eef72112cbc6059498ab2e3924e
David Asensio Cañas <david.asensio@icloud.com>
2022-02-09 21:03:51 +0100

    (examples/react-todos) todoIdListSelector returns an empty list if the state is empty

--- examples/react-todos/src/selectors/todos.js
+++ examples/react-todos/src/selectors/todos.js
@@ -1,4 +1,4 @@
 import { createSelector } from 'reselect'
 
 export const todoIdListSelector =
-  ({ todosById }) => Object.keys(todosById)
+  ({ todosById }) => Object.keys(todosById ?? {})
--- examples/react-todos/src/selectors/todos.test.js
+++ examples/react-todos/src/selectors/todos.test.js
@@ -16,4 +16,12 @@ describe('todoIdListSelector', () => {
       'todo-2'
     ])
   })
+
+  it('should return an empty array if the state is empty', () => {
+    const state = {}
+
+    const result = todoIdListSelector(state)
+
+    expect(result).toStrictEqual([])
+  })
 })
