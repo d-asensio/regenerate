3dc18a4a61bc97f6b90849bad95e40f6ab2a7343
David Asensio Cañas <david.asensio@icloud.com>
2022-02-09 22:05:50 +0100

    (examples/react-todos) The Todo can be toggled

--- examples/react-todos/src/components/Todo.js
+++ examples/react-todos/src/components/Todo.js
@@ -3,11 +3,20 @@ import { Edit, Save, Trash } from 'grommet-icons'
 
 import { useSelector } from '../store'
 import { todoSelector } from '../selectors/todos'
+import { useCallback } from 'react'
+import { run } from '@regenerate/core'
+import { todosService } from '../services/todosService'
 
 export function Todo ({ id }) {
   const todo = useSelector(
     state => todoSelector(state, id)
   )
+
+  const handleCheckboxClick = useCallback(() => {
+    run(
+      todosService.toggleTodo(id)
+    )
+  },[id])
 
   if(!todo) return null
 
@@ -16,7 +25,7 @@ export function Todo ({ id }) {
   return (
     <Card direction="row" pad="medium" gap="medium" fill="horizontal" justify="between" align="center">
       <Box align="center" justify="center" direction="row" gap="small">
-        <CheckBox checked={completed}/>
+        <CheckBox checked={completed} onClick={handleCheckboxClick}/>
         <TextInput value={title} plain={!editing} focusIndicator={!!editing}/>
       </Box>
       <Box align="center" justify="center" direction="row" gap="xxsmall">
