aaa978877415dc134404be610bbbf707c6d6209e
David Asensio Cañas <david.asensio@icloud.com>
2022-02-11 17:39:52 +0100

    (examples/react-todos) Use saveTodosMutation from the todo service

--- examples/react-todos/src/services/todosService.js
+++ examples/react-todos/src/services/todosService.js
@@ -1,23 +1,12 @@
-import keyBy from 'lodash.keyby'
 import http from '../effects/http'
 import store from '../effects/store'
-import { toggleTodoMutation } from '../mutations/todos'
+import { saveTodosMutation, toggleTodoMutation } from '../mutations/todos'
 
 export const todosService = (function IIFE () {
   function * fetchTodos () {
     const todos = yield http.fetchJson('https://jsonplaceholder.typicode.com/todos')
 
-    const todosById = keyBy(
-      todos,
-      ({ id }) => id
-    )
-
-    const todoIdList = Object.keys(todosById)
-
-    yield store.set({
-      todosById,
-      todoIdList
-    })
+    yield store.mutate(saveTodosMutation, todos)
   }
 
   function * toggleTodo (id) {
--- examples/react-todos/src/services/todosService.test.js
+++ examples/react-todos/src/services/todosService.test.js
@@ -1,47 +1,31 @@
 import http from '../effects/http'
 import { todosService } from './todosService'
 import store from '../effects/store'
-import { toggleTodoMutation } from '../mutations/todos'
+import { saveTodosMutation, toggleTodoMutation } from '../mutations/todos'
 
 describe('fetchTodos', () => {
   it('should fetch todos from the API and store them in the store', () => {
+    const todos = [
+      {
+        id: 'a-todo-id',
+        title: 'A todo',
+        completed: true
+      },
+      {
+        id: 'another-todo-id',
+        title: 'Another todo',
+        completed: false
+      }
+    ]
     expect(
       todosService.fetchTodos()
     ).toGenerateEffects([
       {
         effect: http.fetchJson('https://jsonplaceholder.typicode.com/todos'),
-        returns: [
-          {
-            id: 'a-todo-id',
-            title: 'A todo',
-            completed: true
-          },
-          {
-            id: 'another-todo-id',
-            title: 'Another todo',
-            completed: false
-          }
-        ]
+        returns: todos
       },
       {
-        effect: store.set({
-          todosById: {
-            'a-todo-id': {
-              id: 'a-todo-id',
-              title: 'A todo',
-              completed: true
-            },
-            'another-todo-id': {
-              id: 'another-todo-id',
-              title: 'Another todo',
-              completed: false
-            }
-          },
-          todoIdList: [
-            'a-todo-id',
-            'another-todo-id'
-          ]
-        })
+        effect: store.mutate(saveTodosMutation, todos)
       }
     ])
   })
